3eb96965a1feec1ce13cca25724291866ec90dff
Tom Drever <td14@sanger.ac.uk>
2023-08-31 14:40:14 +0100

    Handle empty reads in hts files in the same way

--- src/pyquest/readers/hts.py
+++ src/pyquest/readers/hts.py
@@ -91,10 +91,8 @@ def get_hts_read_info(min_length: int, skip_read_flag: int, read: pysam.AlignedS
 
     # Fetch sequence
     seq: str | None = read.get_forward_sequence()
-    if not seq:
-        raise InputReadError("Error while parsing HTS file: missing sequence!")
 
-    if not is_dna(seq):
+    if seq and not is_dna(seq):
         raise InputReadError(f"Invalid sequence: '{seq}'!")
 
     return ReadInfo(
@@ -107,7 +105,7 @@ def get_hts_read_info(min_length: int, skip_read_flag: int, read: pysam.AlignedS
             'S' in read.cigarstring
         ),
         is_short=(len(seq) < min_length),
-        is_empty=False)
+        is_empty=(not seq))
 
 
 def parse_htsfile(
